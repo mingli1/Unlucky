6dc3a38b5abeff0bf9e4e2081a400aa4ce444893
matamine <mingli434@gmail.com>
2017-11-01 21:59:01 -0400

    First commit

---
+++ android/src/com/unlucky/main/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.unlucky.main;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.unlucky.main.Unlucky;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Unlucky(), config);
+	}
+}
