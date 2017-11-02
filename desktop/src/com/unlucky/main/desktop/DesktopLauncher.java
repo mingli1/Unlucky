723437c39bc4821403c78f11a8fc79db34f06e63
matamine <mingli434@gmail.com>
2017-11-01 21:42:13 -0400

    First commit

---
+++ desktop/src/com/unlucky/main/desktop/DesktopLauncher.java
@@ -0,0 +1,14 @@
+package com.unlucky.main.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.unlucky.main.Unlucky;
+
+public class DesktopLauncher {
+
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Unlucky(), config);
+	}
+
+}
