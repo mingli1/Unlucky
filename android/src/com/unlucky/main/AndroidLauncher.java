390ed2d2a840125f542f17b995617771e6879d7b
matamine <mingli434@gmail.com>
2017-11-03 12:02:49 -0400

    disabled accelerometer and compass for android

--- android/src/com/unlucky/main/AndroidLauncher.java
+++ android/src/com/unlucky/main/AndroidLauncher.java
@@ -15,6 +15,10 @@ public class AndroidLauncher extends AndroidApplication {
 	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+
+        config.useAccelerometer = false;
+        config.useCompass = false;
+
 		initialize(new Unlucky(), config);
 	}
 
