fc7b875822da4e93b02d724aecf6f342fc23e35f
matamine <mingli434@gmail.com>
2017-11-18 10:41:45 -0500

    helper functions

--- core/src/com/unlucky/resource/Util.java
+++ core/src/com/unlucky/resource/Util.java
@@ -1,7 +1,9 @@
 package com.unlucky.resource;
 
+import java.util.Random;
+
 /**
- * Stores useful constants
+ * Stores useful constants and functions
  *
  * @author Ming Li
  */
@@ -16,5 +18,38 @@ public class Util {
     // Directional pad button positions
     public static final int DIR_PAD_SIZE = 40;
     public static final int DIR_PAD_OFFSET = 10;
+
+    // Button dimensions
+    public static final int MOVE_WIDTH = 145;
+    public static final int MOVE_HEIGHT = 50;
+
+    public static final int STD_MED_WIDTH = 110;
+    public static final int STD_MED_HEIGHT = 50;
+
+    // Random
+
+    /**
+     * Returns whether or not an event is successful given a probability in %
+     *
+     * @param p
+     * @param rand
+     * @return
+     */
+    public static boolean isSuccess(int p, Random rand) {
+        int k = rand.nextInt(101);
+        return k <= p;
+    }
+
+    /**
+     * Returns a random number within in a range
+     *
+     * @param n0 min
+     * @param n1 max
+     * @param rand
+     * @return
+     */
+    public static int getRandomValue(int n0, int n1, Random rand) {
+        return rand.nextInt((n1 - n0) + 1) + n0;
+    }
 
 }
