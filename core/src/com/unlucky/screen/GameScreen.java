f3344f71a838f2a2f7cf0bf6fd9fec712860b1d3
matamine <mingli434@gmail.com>
2017-11-22 20:08:22 -0500

    added fsm and input multiplexer to fix conflicting inputs from multiple stages

--- core/src/com/unlucky/screen/GameScreen.java
+++ core/src/com/unlucky/screen/GameScreen.java
@@ -1,14 +1,17 @@
 package com.unlucky.screen;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.math.Vector2;
 import com.unlucky.entity.Player;
 import com.unlucky.entity.monsters.Slime;
+import com.unlucky.event.EventState;
 import com.unlucky.main.Unlucky;
 import com.unlucky.map.TileMap;
 import com.unlucky.resource.ResourceManager;
-import com.unlucky.scene.Hud;
+import com.unlucky.ui.BattleUI;
+import com.unlucky.ui.Hud;
 
 /**
  * Handles all gameplay.
@@ -17,29 +20,44 @@ import com.unlucky.scene.Hud;
  */
 public class GameScreen extends AbstractScreen {
 
+    private EventState currentEvent;
+
     private TileMap map;
     private Player player;
     private Hud hud;
+    private BattleUI battle;
 
     public GameScreen(final Unlucky game, final ResourceManager rm) {
         super(game, rm);
 
+        currentEvent = EventState.MOVING;
+
         map = new TileMap(16, "maps/test_map.txt", new Vector2(0, 0), rm);
         player = new Player("player", map.toMapCoords(5, 8), map, rm);
         hud = new Hud(player, game.batch, rm);
+        battle = new BattleUI(player, game.batch, rm);
+
+        // input multiplexer
+        InputMultiplexer multiplexer = new InputMultiplexer();
+        multiplexer.addProcessor(hud.stage);
+        multiplexer.addProcessor(battle.stage);
+        Gdx.input.setInputProcessor(multiplexer);
 
         map.getTile(4, 4).addEntity(new Slime("slime", map.toMapCoords(4, 4), map, rm));
     }
 
     public void update(float dt) {
-        // camera directs on the player
-        cam.position.x = player.getPosition().x + 8;
-        cam.position.y = player.getPosition().y + 4;
+        if (currentEvent == EventState.MOVING) {
+            // camera directs on the player
+            cam.position.x = player.getPosition().x + 8;
+            cam.position.y = player.getPosition().y + 4;
+            cam.update();
 
-        player.update(dt);
-        map.update(dt);
+            player.update(dt);
+            map.update(dt);
+        }
 
-        cam.update();
+        if (currentEvent == EventState.BATTLING) battle.update(dt);
     }
 
     public void render(float dt) {
@@ -51,18 +69,29 @@ public class GameScreen extends AbstractScreen {
         game.batch.setProjectionMatrix(cam.combined);
         game.batch.begin();
 
-        map.render(game.batch);
-        player.render(game.batch);
+        if (currentEvent == EventState.MOVING) {
+            map.render(game.batch);
+            player.render(game.batch);
+        }
 
         game.batch.end();
 
-        hud.stage.act(dt);
-        hud.stage.draw();
+        if (currentEvent == EventState.MOVING) hud.render(dt);
+        if (currentEvent == EventState.BATTLING) battle.render(dt);
     }
 
     public void dispose() {
         super.dispose();
         hud.dispose();
+        battle.dispose();
+    }
+
+    /**
+     * @TODO: Add some sort of transitioning between events
+     * @param event
+     */
+    public void setCurrentEvent(EventState event) {
+        this.currentEvent = event;
     }
 
 }
