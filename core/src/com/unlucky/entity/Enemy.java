474dac24594c31716d1f0a9180973bc9d4f2964a
matamine <mingli434@gmail.com>
2017-12-07 12:01:39 -0500

    made specific constructor

--- core/src/com/unlucky/entity/Enemy.java
+++ core/src/com/unlucky/entity/Enemy.java
@@ -1,6 +1,7 @@
 package com.unlucky.entity;
 
 import com.badlogic.gdx.math.Vector2;
+import com.unlucky.animation.AnimationManager;
 import com.unlucky.map.TileMap;
 import com.unlucky.resource.ResourceManager;
 
@@ -24,12 +25,43 @@ public class Enemy extends Entity {
         speed = 0;
     }
 
+    /**
+     * Creates a specific enemy with animation
+     *
+     * @param id
+     * @param position
+     * @param tileMap
+     * @param rm
+     * @param numFrames
+     * @param animIndex
+     * @param delay
+     */
+    public Enemy(String id, Vector2 position, TileMap tileMap, ResourceManager rm,
+                 int numFrames, int animIndex, float delay)
+    {
+        super(id, position, tileMap, rm);
+        speed = 0;
+        am = new AnimationManager(rm.sprites16x16, numFrames, animIndex, delay);
+    }
+
     public int getExpDrop() {
         return expDrop;
     }
 
     public int getGoldDrop() {
         return goldDrop;
+    }
+
+    public void setMaxHp(int maxHp) {
+        this.maxHp = maxHp;
+    }
+
+    public void setMinDamage(int minDamage) {
+        this.minDamage = minDamage;
+    }
+
+    public void setMaxDamage(int maxDamage) {
+        this.maxDamage = maxDamage;
     }
 
 }
