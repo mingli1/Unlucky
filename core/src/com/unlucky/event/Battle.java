66deb3a790026512270e2a28fd2a256cc677e495
matamine <mingli434@gmail.com>
2017-12-17 13:51:37 -0500

    enemy calc

--- core/src/com/unlucky/event/Battle.java
+++ core/src/com/unlucky/event/Battle.java
@@ -3,6 +3,7 @@ package com.unlucky.event;
 import com.unlucky.entity.Enemy;
 import com.unlucky.entity.Player;
 import com.unlucky.map.TileMap;
+import com.unlucky.resource.Util;
 import com.unlucky.screen.GameScreen;
 
 /**
@@ -35,11 +36,31 @@ public class Battle {
 
     /**
      * Sets and scales the enemy's stats according to how strong the player is
+     * If the enemy is a boss, then its stats are between 1.3-1.8x higher
+     * @TODO Scale enemies based on level of map
+     * @TODO exp and gold calculations
      *
      * @param opponent
      */
     public void begin(Enemy opponent) {
         this.opponent = opponent;
+
+        float bossMultiplier = (player.getRandom().nextFloat() * 0.5f) + 1.3f;
+
+        // choose damage num from player's damage range
+        int seed = Util.getRandomValue(player.getMinDamage(), player.getMaxDamage(), player.getRandom());
+        // an enemy will take around 3-6 hits to defeat
+        // maxHp = rand(3, 6) * seed
+        int multiplier = Util.getRandomValue(3, 6, player.getRandom());
+        opponent.setMaxHp(opponent.isBoss() ? (int) bossMultiplier * (seed * multiplier) : seed * multiplier);
+
+        // the estimated num of hits for an enemy to kill the player, between 8 and 12 scaled for move damage
+        int numHitsToKill = Util.getRandomValue(8, 12, player.getRandom());
+        int dmgSeed = player.getMaxHp() / numHitsToKill;
+        // enemy's damage range is scaled to dmgSeed +- 4-6x dmgSeed
+        int sigma = dmgSeed / Util.getRandomValue(4, 6, player.getRandom());
+        opponent.setMinDamage(opponent.isBoss() ? (int) bossMultiplier * (dmgSeed - sigma) : dmgSeed - sigma);
+        opponent.setMaxDamage(opponent.isBoss() ? (int) bossMultiplier * (dmgSeed + sigma) : dmgSeed + sigma);
     }
 
     /**
@@ -50,6 +71,7 @@ public class Battle {
         tileMap.removeEntity(tileMap.toTileCoords(player.getPosition()));
         player.finishBattling();
         gameScreen.setCurrentEvent(EventState.MOVING);
+        gameScreen.hud.toggle(true);
     }
 
 }
