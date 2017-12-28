38b598c4ae5c6c5bebeff8ccd3c02e1571a1a025
matamine <mingli434@gmail.com>
2017-12-27 19:40:41 -0500

    level up and exp handling

--- core/src/com/unlucky/entity/Player.java
+++ core/src/com/unlucky/entity/Player.java
@@ -19,6 +19,18 @@ public class Player extends Entity {
     private Enemy opponent;
     private boolean battling = false;
 
+    // exp and level up
+    private int exp;
+    private int maxExp;
+
+    private int hpIncrease = 0;
+    private int minDmgIncrease = 0;
+    private int maxDmgIncrease = 0;
+    private int accuracyIncrease = 0;
+
+    // gold
+    private int gold = 0;
+
     public Player(String id, Vector2 position, TileMap tileMap, ResourceManager rm) {
         super(id, position, tileMap, rm);
 
@@ -31,7 +43,15 @@ public class Player extends Entity {
         level = 1;
         speed = 1;
 
+        exp = 0;
+        // offset between 3 and 5
+        maxExp = Util.calculateMaxExp(1, Util.getRandomValue(3, 5, rand));
+
+        // create tilemap animation
         am = new AnimationManager(rm.sprites16x16, Util.PLAYER_WALKING, Util.PLAYER_WALKING_DELAY);
+        // create battle scene animation
+        bam = new AnimationManager(rm.battleSprites96x96, 2, Util.PLAYER_WALKING, 2 / 5f);
+
         moveset = new Moveset(rm);
         // damage seed is a random number between the damage range
         moveset.reset(minDamage, maxDamage, maxHp);
@@ -51,7 +71,51 @@ public class Player extends Entity {
     }
 
     public void render(SpriteBatch batch) {
+        // draw shadow
+        batch.draw(rm.shadow11x6, position.x + 3, position.y - 3);
         batch.draw(am.getKeyFrame(true), position.x + 1, position.y);
+    }
+
+    /**
+     * Increments level and recalculates max exp
+     * Sets increase variables to display on screen
+     * Recursively accounts for n consecutive level ups from remaining exp
+     *
+     * @param remainder the amount of exp left after a level up
+     */
+    public void levelUp(int remainder) {
+        level++;
+
+        hpIncrease += Util.getRandomValue(Util.PLAYER_MIN_HP_INCREASE, Util.PLAYER_MAX_HP_INCREASE, rand);
+        minDmgIncrease += Util.getRandomValue(Util.PLAYER_MIN_DMG_INCREASE, Util.PLAYER_MAX_DMG_INCREASE, rand);
+        maxDmgIncrease += Util.getRandomValue(Util.PLAYER_MIN_DMG_INCREASE, Util.PLAYER_MAX_DMG_INCREASE, rand);
+        // accuracy increases by 1% every 10 levels
+        accuracyIncrease += level % 10 == 0 ? 1 : 0;
+
+        maxExp = Util.calculateMaxExp(level, Util.getRandomValue(3, 5, rand));
+        // another level up
+        if (remainder >= maxExp) {
+            levelUp(remainder - maxExp);
+        } else {
+            exp = remainder;
+        }
+    }
+
+    /**
+     * Increases the actual stats by their level up amounts
+     */
+    public void applyLevelUp() {
+        maxHp += hpIncrease;
+        hp = maxHp;
+        minDamage += minDmgIncrease;
+        maxDamage += maxDmgIncrease;
+        accuracy += accuracyIncrease;
+
+        // reset variables
+        hpIncrease = 0;
+        minDmgIncrease = 0;
+        maxDmgIncrease = 0;
+        accuracyIncrease = 0;
     }
 
     public Enemy getOpponent() {
@@ -65,6 +129,58 @@ public class Player extends Entity {
 
     public boolean isBattling() {
         return battling;
+    }
+
+    public void setExp(int exp) {
+        this.exp = exp;
+    }
+
+    public void addExp(int exp) {
+        this.exp += exp;
+    }
+
+    public void setMaxExp(int maxExp) {
+        this.maxExp = maxExp;
+    }
+
+    public int getExp() {
+        return exp;
+    }
+
+    public int getMaxExp() {
+        return maxExp;
+    }
+
+    public int getHpIncrease() {
+        return hpIncrease;
+    }
+
+    public void setHpIncrease(int hpIncrease) {
+        this.hpIncrease = hpIncrease;
+    }
+
+    public int getMinDmgIncrease() {
+        return minDmgIncrease;
+    }
+
+    public void setMinDmgIncrease(int minDmgIncrease) {
+        this.minDmgIncrease = minDmgIncrease;
+    }
+
+    public int getMaxDmgIncrease() {
+        return maxDmgIncrease;
+    }
+
+    public void setMaxDmgIncrease(int maxDmgIncrease) {
+        this.maxDmgIncrease = maxDmgIncrease;
+    }
+
+    public int getAccuracyIncrease() {
+        return accuracyIncrease;
+    }
+
+    public void setAccuracyIncrease(int accuracyIncrease) {
+        this.accuracyIncrease = accuracyIncrease;
     }
 
 }
