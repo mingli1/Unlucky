60b098597af3b3ea1dcfb91e6a7d6877781e6fbf
matamine <mingli434@gmail.com>
2018-03-22 12:46:13 -0400

    unscaled

--- core/src/com/unlucky/screen/MenuScreen.java
+++ core/src/com/unlucky/screen/MenuScreen.java
@@ -53,10 +53,6 @@ public class MenuScreen extends AbstractScreen {
     public MenuScreen(final Unlucky game, final ResourceManager rm) {
         super(game, rm);
 
-        // Override
-        viewport = new ExtendViewport(Unlucky.V_WIDTH * 2, Unlucky.V_HEIGHT * 2, new OrthographicCamera());
-        stage = new Stage(viewport, game.batch);
-
         // background
         createBackground();
 
@@ -101,10 +97,10 @@ public class MenuScreen extends AbstractScreen {
         bg[0].setVector(0, 0);
         // back clouds
         bg[1] = new Background(rm.titleScreenBackground[2], (OrthographicCamera) stage.getCamera(), new Vector2(0.3f, 0), 2);
-        bg[1].setVector(40, 0);
+        bg[1].setVector(20, 0);
         // front clouds
         bg[2] = new Background(rm.titleScreenBackground[1], (OrthographicCamera) stage.getCamera(), new Vector2(0.3f, 0), 2);
-        bg[2].setVector(120, 0);
+        bg[2].setVector(60, 0);
     }
 
     private void handlePlayButton() {
@@ -112,7 +108,7 @@ public class MenuScreen extends AbstractScreen {
         s.imageUp = new TextureRegionDrawable(rm.playButton[0][0]);
         s.imageDown = new TextureRegionDrawable(rm.playButton[1][0]);
         playButton = new ImageButton(s);
-        playButton.setPosition(120, 70);
+        playButton.setPosition(60, 35);
         stage.addActor(playButton);
         playButton.addListener(new ClickListener() {
             @Override
@@ -138,22 +134,22 @@ public class MenuScreen extends AbstractScreen {
         optionButtons = new ImageButton[NUM_BUTTONS];
         for (int i = 0; i < NUM_BUTTONS; i++) {
             optionButtons[i] = new ImageButton(styles[i]);
-            optionButtons[i].setSize(40, 40);
+            optionButtons[i].setSize(20, 20);
             optionButtons[i].getImage().setFillParent(true);
             stage.addActor(optionButtons[i]);
         }
         // inventory button
-        optionButtons[0].setPosition(12, 170);
+        optionButtons[0].setPosition(6, 85);
         // settings button
-        optionButtons[1].setPosition(342, 170);
+        optionButtons[1].setPosition(171, 85);
         // shop button
-        optionButtons[2].setPosition(12, 100);
+        optionButtons[2].setPosition(6, 50);
         // smove button
-        optionButtons[3].setPosition(12, 30);
+        optionButtons[3].setPosition(6, 15);
         // statistics button
-        optionButtons[4].setPosition(340, 100);
+        optionButtons[4].setPosition(170, 50);
         // credits button
-        optionButtons[5].setPosition(340, 30);
+        optionButtons[5].setPosition(170, 15);
     }
 
     public void update(float dt) {
@@ -168,7 +164,6 @@ public class MenuScreen extends AbstractScreen {
     }
 
     public void render(float dt) {
-        super.render(dt);
         update(dt);
 
         // clear screen
@@ -183,8 +178,7 @@ public class MenuScreen extends AbstractScreen {
             stage.getBatch().end();
         }
 
-        stage.act(dt);
-        stage.draw();
+        super.render(dt);
     }
 
     /**
@@ -193,9 +187,9 @@ public class MenuScreen extends AbstractScreen {
     private void resetTitleAnimation() {
         // entire title text starts at x = 74
         for (int i = 0; i < titleMoves.length; i++) {
-            titleMoves[i].origin.set(new Vector2(74 + i * 36, 240 + 48));
-            titleMoves[i].target.set(new Vector2(74 + i * 36, 240 - 70));
-            titleMoves[i].speed = 275 - i * 24;
+            titleMoves[i].origin.set(new Vector2(37 + i * 18, 120 + 24));
+            titleMoves[i].target.set(new Vector2(37 + i * 18, 120 - 35));
+            titleMoves[i].speed = (275 - i * 24) / 2;
             titleMoves[i].horizontal = false;
             titleMoves[i].start();
         }
