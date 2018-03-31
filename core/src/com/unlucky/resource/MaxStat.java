c891f7382f998d4971c7afccda52c88a009f9632
matamine <mingli434@gmail.com>
2018-03-31 00:40:57 -0400

    more stats

---
+++ core/src/com/unlucky/resource/MaxStat.java
@@ -0,0 +1,17 @@
+package com.unlucky.resource;
+
+/**
+ * Represents a "max" statistic
+ *
+ * @author Ming Li
+ */
+public class MaxStat {
+
+    public int stat;
+
+    public MaxStat() {
+        stat = 0;
+    }
+
+}
+
