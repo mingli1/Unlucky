249103944b42f89b24072109dc6dc1422060db1f
matamine <mingli434@gmail.com>
2018-04-03 21:08:28 -0400

    world data

---
+++ core/src/com/unlucky/map/World.java
@@ -0,0 +1,28 @@
+package com.unlucky.map;
+
+/**
+ * A World has a theme, lore, and boss
+ * Each world has a certain number of levels with the boss appearing at the end
+ *
+ * @author Ming Li
+ */
+public class World {
+
+    // name of the world
+    public String name;
+    // description to be displayed on the world's button
+    public String shortDesc;
+    // description of the lore, theme, etc
+    public String longDesc;
+
+    // the number of levels in the world
+    public int numLevels;
+
+    public World(String name, String shortDesc, String longDesc, int numLevels) {
+        this.name = name;
+        this.shortDesc = shortDesc;
+        this.longDesc = longDesc;
+        this.numLevels = numLevels;
+    }
+
+}
