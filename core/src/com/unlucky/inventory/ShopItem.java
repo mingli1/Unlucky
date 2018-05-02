58b1a04bcec9b89a7df29582f09b50689f773a39
matamine <mingli434@gmail.com>
2018-05-02 00:25:58 -0400

    shop item

--- core/src/com/unlucky/inventory/ShopItem.java
+++ core/src/com/unlucky/inventory/ShopItem.java
@@ -1,14 +1,53 @@
 package com.unlucky.inventory;
 
+import com.unlucky.resource.ResourceManager;
+
 /**
  * A special type of item that is only sold in the shop
  * and has a certain cost in gold
  *
  * @author Ming Li
  */
-public class ShopItem {
+public class ShopItem extends Item {
 
     // price of the item in the shop
     public int price;
+
+    /**
+     * For potions
+     * Only can be consumed for hp or sold for gold
+     *
+     * @param name
+     * @param desc
+     * @param rarity
+     * @param imgIndex for textureregion in spritesheet
+     * @param hp
+     * @param sell
+     */
+    public ShopItem(ResourceManager rm, String name, String desc, int rarity,
+                    int imgIndex, int hp, int exp, int sell, int price) {
+        super(rm, name, desc, rarity, imgIndex, hp, exp, sell);
+        this.price = price;
+    }
+
+    /**
+     * For all types of equips
+     * Gives increased stats and can be sold for gold
+     *
+     * @param name
+     * @param desc
+     * @param type
+     * @param rarity
+     * @param imgIndex
+     * @param mhp
+     * @param dmg
+     * @param acc
+     * @param sell
+     */
+    public ShopItem(ResourceManager rm, String name, String desc, int type, int rarity,
+                    int imgIndex, int mhp, int dmg, int acc, int sell, int price) {
+        super(rm, name, desc, type, rarity, imgIndex, mhp, dmg, acc, sell);
+        this.price = price;
+    }
 
 }
