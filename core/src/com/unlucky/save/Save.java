ac2a81c9fb7c44a05a860d27c0838cada2f81be2
matamine <mingli434@gmail.com>
2018-05-11 16:56:58 -0400

    save system finished

--- core/src/com/unlucky/save/Save.java
+++ core/src/com/unlucky/save/Save.java
@@ -4,7 +4,13 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.JsonWriter;
+import com.unlucky.battle.SpecialMoveset;
 import com.unlucky.entity.Player;
+import com.unlucky.inventory.Equipment;
+import com.unlucky.inventory.Inventory;
+import com.unlucky.inventory.Item;
+import com.unlucky.inventory.ShopItem;
+import com.unlucky.resource.ResourceManager;
 
 /**
  * Handles the reading and writing of save data to json files.
@@ -37,6 +43,121 @@ public class Save {
         psave.load(player);
         // write data to save json
         file.writeString(json.prettyPrint(psave), false);
+    }
+
+    /**
+     * Reads the player data from the save json file and then
+     * loads the data into the game through the player
+     */
+    public void load(ResourceManager rm) {
+        if (!file.exists()) save();
+        psave = json.fromJson(PlayerAccessor.class, file);
+
+        // load atomic fields
+        player.setHp(psave.hp);
+        player.setMaxHp(psave.maxHp);
+        player.setLevel(psave.level);
+        player.setExp(psave.exp);
+        player.setMaxExp(psave.maxExp);
+        player.setGold(psave.gold);
+        player.setMinDamage(psave.minDamage);
+        player.setMaxDamage(psave.maxDamage);
+        player.setAccuracy(psave.accuracy);
+        player.smoveCd = psave.smoveCd;
+
+        // load inventory and equips
+        loadInventory(rm);
+        loadEquips(rm);
+
+        // load smoveset
+        for (int i = 0; i < SpecialMoveset.MAX_MOVES; i++) {
+            if (psave.smoveset[i] != -1) {
+                player.smoveset.addSMove(psave.smoveset[i]);
+            }
+        }
+
+        // load statistics
+        player.stats = psave.stats;
+    }
+
+    /**
+     * Helper method for loading and converting ItemAccessors to Items in the inventory
+     */
+    private void loadInventory(ResourceManager rm) {
+        for (int i = 0; i < Inventory.NUM_SLOTS; i++) {
+            ItemAccessor ia = psave.inventory[i];
+            if (ia != null) {
+                // shop items
+                if (ia instanceof ShopItemAccessor) {
+                    ShopItem sitem = null;
+                    if (ia.type == 0)
+                        sitem = new ShopItem(rm, ia.name, ia.desc, ia.rarity, ia.imgIndex,
+                            ia.hp, ia.exp, ia.sell, ((ShopItemAccessor) ia).price);
+                    else if (ia.type >= 2 && ia.type <= 9)
+                        sitem = new ShopItem(rm, ia.name, ia.desc, ia.type, ia.rarity, ia.imgIndex,
+                            ia.mhp, ia.dmg, ia.acc, ia.sell, ((ShopItemAccessor) ia).price);
+                    else if (ia.type == 10)
+                        sitem = new ShopItem(rm, ia.name, ia.desc, ia.rarity, ia.imgIndex,
+                            ia.eChance, ia.sell, ((ShopItemAccessor) ia).price);
+                    player.inventory.addItemAtIndex(sitem, ia.index);
+                }
+                else {
+                    Item item = null;
+                    if (ia.type == 0)
+                        item = new Item(rm, ia.name, ia.desc, ia.rarity, ia.imgIndex,
+                            ia.hp, ia.exp, ia.sell);
+                    else if (ia.type == 1)
+                        item = new Item(rm, ia.name, ia.desc, ia.rarity, ia.imgIndex, ia.sell);
+                    else if (ia.type >= 2 && ia.type <= 9)
+                        item = new Item(rm, ia.name, ia.desc, ia.type, ia.rarity, ia.imgIndex,
+                            ia.mhp, ia.dmg, ia.acc, ia.sell);
+                    else if (ia.type == 10)
+                        item = new Item(rm, ia.name, ia.desc, ia.rarity, ia.imgIndex,
+                            ia.eChance, ia.sell);
+                    player.inventory.addItemAtIndex(item, ia.index);
+                }
+            }
+        }
+    }
+
+    /**
+     * Helper method for loading and converting ItemAccessors to Items in the inventory
+     */
+    private void loadEquips(ResourceManager rm) {
+        for (int i = 0; i < Equipment.NUM_SLOTS; i++) {
+            ItemAccessor ia = psave.equips[i];
+            if (ia != null) {
+                // shop items
+                if (ia instanceof ShopItemAccessor) {
+                    ShopItem sitem = null;
+                    if (ia.type == 0)
+                        sitem = new ShopItem(rm, ia.name, ia.desc, ia.rarity, ia.imgIndex,
+                            ia.hp, ia.exp, ia.sell, ((ShopItemAccessor) ia).price);
+                    else if (ia.type >= 2 && ia.type <= 9)
+                        sitem = new ShopItem(rm, ia.name, ia.desc, ia.type, ia.rarity, ia.imgIndex,
+                            ia.mhp, ia.dmg, ia.acc, ia.sell, ((ShopItemAccessor) ia).price);
+                    else if (ia.type == 10)
+                        sitem = new ShopItem(rm, ia.name, ia.desc, ia.rarity, ia.imgIndex,
+                            ia.eChance, ia.sell, ((ShopItemAccessor) ia).price);
+                    player.equips.addEquip(sitem);
+                }
+                else {
+                    Item item = null;
+                    if (ia.type == 0)
+                        item = new Item(rm, ia.name, ia.desc, ia.rarity, ia.imgIndex,
+                            ia.hp, ia.exp, ia.sell);
+                    else if (ia.type == 1)
+                        item = new Item(rm, ia.name, ia.desc, ia.rarity, ia.imgIndex, ia.sell);
+                    else if (ia.type >= 2 && ia.type <= 9)
+                        item = new Item(rm, ia.name, ia.desc, ia.type, ia.rarity, ia.imgIndex,
+                            ia.mhp, ia.dmg, ia.acc, ia.sell);
+                    else if (ia.type == 10)
+                        item = new Item(rm, ia.name, ia.desc, ia.rarity, ia.imgIndex,
+                            ia.eChance, ia.sell);
+                    player.equips.addEquip(item);
+                }
+            }
+        }
     }
 
 }
