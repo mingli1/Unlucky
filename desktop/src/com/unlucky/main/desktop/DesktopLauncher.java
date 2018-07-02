e2008817d54b07d0a0a2ccd7b5c7bca924522657
matamine <mingli434@gmail.com>
2018-07-02 17:59:09 -0400

    added desktop icons

--- desktop/src/com/unlucky/main/desktop/DesktopLauncher.java
+++ desktop/src/com/unlucky/main/desktop/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.unlucky.main.desktop;
 
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.unlucky.main.Unlucky;
@@ -13,10 +14,14 @@ public class DesktopLauncher {
         LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
         config.width = Unlucky.V_WIDTH * Unlucky.V_SCALE;
         config.height = Unlucky.V_HEIGHT * Unlucky.V_SCALE;
+        config.title = Unlucky.TITLE;
         config.resizable = false;
         config.vSyncEnabled = false;
         config.backgroundFPS = 10;
         config.foregroundFPS = 60;
+        config.addIcon("desktop_icon128.png", Files.FileType.Internal);
+        config.addIcon("desktop_icon32.png", Files.FileType.Internal);
+        config.addIcon("desktop_icon16.png", Files.FileType.Internal);
         new LwjglApplication(new Unlucky(), config);
     }
 
